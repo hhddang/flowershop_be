773661ca540416d8cc6b45bd0b2d28028a1c48cf
hhddang <dangtramthien@gmail.com>
2023-10-03 21:07:12 +0700

    Init

---
+++ src/main/java/springboot/flowershop_be/FlowershopBeApplication.java
@@ -0,0 +1,13 @@
+package springboot.flowershop_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowershopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowershopBeApplication.class, args);
+	}
+
+}
